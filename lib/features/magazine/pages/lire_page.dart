d9475fbff34342091e6151248191d711f10b50ca
PharmaConsults Expertise <pharmaconsults@192.168.1.5>
2025-08-13 01:37:39 +0000

    update logo and read pdf

--- lib/features/magazine/pages/lire_page.dart
+++ lib/features/magazine/pages/lire_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:http/http.dart' as http;
+import 'package:pdfx/pdfx.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../../core/themes/themes.dart';
@@ -12,6 +13,43 @@ class LirePage extends StatefulWidget {
 }
 
 class _LirePageState extends State<LirePage> {
+
+  PdfController? _pdfController;
+  bool _loading = true;
+  final String pdfUrl = "https://www.example.com/monfichier.pdf";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPdfFromUrl();
+  }
+
+  Future<void> _loadPdfFromUrl() async {
+    try {
+      // Télécharger le PDF depuis l'URL
+      final response = await http.get(Uri.parse(pdfUrl));
+      if (response.statusCode == 200) {
+        setState(() {
+          _pdfController = PdfController(
+            document: PdfDocument.openData(response.bodyBytes),
+            initialPage: 1,
+          );
+          _loading = false;
+        });
+      } else {
+        throw Exception("Erreur de téléchargement du PDF");
+      }
+    } catch (e) {
+      debugPrint("Erreur: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _pdfController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +57,11 @@ class _LirePageState extends State<LirePage> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(4.w),
-          child: ListView(
+          child: _loading
+              ? const Center(child: CircularProgressIndicator())
+              : PdfView(
+            controller: _pdfController!,
             scrollDirection: Axis.horizontal,
-            children: [
-              Image.asset("assets/magas/maga.jpg", ),
-              Image.asset("assets/magas/maga.jpg", ),
-            ],
           ),
         ),
       ),
